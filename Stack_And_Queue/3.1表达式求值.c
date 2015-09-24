3ec1592eea0d506c0dab412a0eaa349bee5ec51e
zhuxinquan <1174272148@qq.com>
2015-09-24 17:42:01 +0800

    表达式求值

---
+++ Stack_And_Queue/3.1表达式求值.c
@@ -0,0 +1,206 @@
+/*************************************************************************
+	> File Name: 3.1表达式求值.c
+	> Author: zhuxinquan
+	> Mail: zhuxinquan61@gmail.com
+	> Created Time: 2015年09月22日 星期二 16时04分13秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+
+#define FALSE 0
+#define TRUE 1
+
+typedef struct stacknode{
+    int data;
+    struct stacknode * next;
+}StackType;
+
+int get_stack(StackType *, int *);             //取栈顶元素
+int pop_stack(StackType *, int *);             //出栈
+int Push_stack(StackType *, int);       //入栈
+char compare(char, char);               //比较运算符优先级
+int Operation(int , char, int );        //实际运算函数
+
+StackType * Init_stack()
+{
+    StackType * top;
+    top = (StackType *)malloc(sizeof(StackType));
+    top->next = NULL;
+    return top;
+}
+
+int Push_stack(StackType * top, int x)
+{
+    StackType * p;
+    p = (StackType *)malloc(sizeof(StackType));
+    if(p == NULL){
+        return FALSE;
+    }
+    p->data = x;
+    p->next = top->next;
+    top->next = p;
+    return TRUE;
+}
+
+int pop_stack(StackType * top, int * x)
+{
+    StackType * p;
+    if(top->next == NULL){
+        return -1;
+    }
+    p = top->next;
+    top->next = p->next;
+    *x = p->data;
+    free(p);
+    return 0;
+}
+
+int getnext(int * n)                        //该函数返回0为数字，返回1为运算符
+{
+    char c;
+    *n = 0;
+    while((c = getchar()) == ' ');          //跳过空格
+    if(!isdigit(c)){                        //判断是否是数字
+        *n = c;
+        return 1;
+    }
+    do{
+        *n = *n * 10 + (c - '0');           //使用循环获得连续的数字，乘10进位
+        c = getchar();
+    }while(isdigit(c));
+    ungetc(c, stdin);                       //读到一个运算符，将运算符写回输入缓存
+    return 0;
+}
+
+/*对于连续出现的运算符进行比较优先级
+ * 结果有> < =，可以得到+-×/之间的优
+ * 先级，加减乘除的优先级都低于'('，
+ * 但是都高于')',并且根据从左到右运算
+ * 可知当运算符相同时，第一个大于第二
+ * 个,0表示不存在
+ * */
+
+char compare(char a, char b)
+{
+    int i, j;
+    char pre[7][7] ={                       //定义运算符之间的优先级
+        {'>','>','<','<','<','>','>'},
+        {'>','>','<','<','<','>','>'},
+        {'>','>','>','>','<','>','>'},
+        {'>','>','>','>','<','>','>'},
+        {'<','<','<','<','<','=','0'},
+        {'>','>','>','>','0','>','>'},
+        {'<','<','<','<','<','0','='},
+    };
+    switch(a){
+        case '+': i = 0; break;
+        case '-': i = 1; break;
+        case '*': i = 2; break;
+        case '/': i = 3; break;
+        case '(': i = 4; break;
+        case ')': i = 5; break;
+        case '#': i = 6; break;
+    }
+    switch(b){
+        case '+': j = 0; break;
+        case '-': j = 1; break;
+        case '*': j = 2; break;
+        case '/': j = 3; break;
+        case '(': j = 4; break;
+        case ')': j = 5; break;
+        case '#': j = 6; break;
+    }
+    return pre[i][j];
+}
+
+int Operation(int a, char operate, int b)           //实际操作两个数a,b的运算
+{
+    int i, j, result;
+    i = a;
+    j = b;
+
+    switch(operate){
+        case '+': result = a + b; break;
+        case '-': result = a - b; break;
+        case '*': result = a * b; break;
+        case '/': result = a / b; break;
+    }
+    return result;
+}
+
+int EvaluateExpression()
+{
+    int n;
+    int c;                                  //存储输入缓存中的字符或数字
+    int flag;                               //从输入缓存中取操作符的返回值，0表示取出数字，1表示取出运算符
+    int x;                                  //取栈顶操作符
+    int operate;                            //存储要计算的操作符
+    int a, b;                               //存取要计算的操作数
+    StackType * top_oprd, * top_optr;       //top_oprd为操作数栈，top_optr为运算符栈
+    
+    char op[] = "+-*/()#";
+
+    top_oprd = Init_stack();
+    top_optr = Init_stack();
+    
+    Push_stack(top_optr, '#');
+    flag = getnext(&c);
+    
+    get_stack(top_optr, &x);
+    while(c != '#' || x != '#')         //表达式的起始位置都是'#'，如果读取的新的字符和运算符都是'#'说明运算已经结束
+    {
+        if(flag == 0)                   //返回数字
+        {
+            Push_stack(top_oprd, c);
+            flag = getnext(&c);
+        }
+        else                            //返回运算符
+        {
+            get_stack(top_optr, &x);        //取栈顶运算符
+            switch(compare(x, c))
+            {
+                case '<':               //栈顶操作符运算优先级低
+                Push_stack(top_optr, c);
+                flag = getnext(&c);
+                break;
+                case '>':               //栈顶运算符优先级高,先退出两个数据进行运算，然后将运算结果在存入数据栈
+                pop_stack(top_optr, &operate);
+                pop_stack(top_oprd, &a);
+                pop_stack(top_oprd, &b);
+                Push_stack(top_oprd, Operation(a, operate, b));
+                break;
+                case '=':               //操作符'('')'紧挨，则直接去除括号
+                pop_stack(top_optr, &operate);
+                flag = getnext(&c);
+                break;
+                case '0':               //比较结果得出表达式错误
+                printf("input error!\n");
+            }
+        }
+        get_stack(top_optr, &x);
+    }
+    get_stack(top_oprd, &c);
+    return c;
+
+}
+
+int get_stack(StackType * top, int * x)
+{
+    if(top->next == NULL)
+    {
+        return -1;
+    }
+    *x = top->next->data;
+    return 0;
+}
+
+int main(void)
+{
+    int c;
+    printf("please input expression:");
+    c = EvaluateExpression();
+    printf("result = %d\n", c);
+    getchar();
+}
